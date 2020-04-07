22cfbac422711776770c71135625c6ad60f42d44
Emerson Costa <syphonemerson@yahoo.com.br>
2020-04-06 21:55:06 -0300

    Aula 7 Exercicio

---
+++ Aula 7/Exercicio/retornar_lista.php
@@ -0,0 +1,9 @@
+<?php
+
+$lista = file_get_contents("lista.html");
+
+echo $lista;
+
+return;
+
+?>
---
+++ Aula 7/Exercicio/write.php
@@ -0,0 +1,8 @@
+﻿<?php
+
+if(isset($_GET['lista'])) {
+    file_put_contents('lista.html', $_GET['lista']);
+    exit();
+}
+
+?>
